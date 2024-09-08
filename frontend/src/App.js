ed194d8cb1569550cd2456f40c17a62835005b32
Oleksandr <alexklimovv2@gmail.com>
2024-09-08 23:16:50 +0300

    react navbar + sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,25 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import {Route, Router, Routes} from "react-router-dom";
+import React from "react";
+import "./styles.css";
 
-const App = () => {
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
+// PAGES
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/Home";
+import Products from "./components/Products";
+import Reports from "./components/Reports";
+
+export default function App() {
     return (
-        <Router>
-            <Routes>
-                
-            </Routes>
-        </Router>
+        <div className="App">
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/products"  element={<Products/>} />
+                    <Route path="/reports"  element={<Reports/>} />
+                </Routes>
+            </Router>
+        </div>
     );
-};
-
-export default App;
+}
